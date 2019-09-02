d9354c4b9b4d617fe16652f63d5f964562618a60
Ruel Tingling <rvtingling@yahoo.com>
2019-09-02 09:00:21 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,13 +2,13 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        #your key/value pairs here
+      name: "lord montague", age: "53"
         },
       :matriarch => {
-        #your key/value pairs here
+         name: "lady montague", age: "54"
         },
       :hero => {
-        #your key/value pairs here
+         name: "Romeo", age: "15", status: "alive"
         },
       :hero_friends => []
    }, 
